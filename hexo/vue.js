61cc1c8a3a4ff247093b7383819472343e12742f
OpenTritium <tritium404@hotmail.com>
2022-11-14 00:22:24 +0800

    Delete vue.js

--- hexo/vue.js
+++
@@ -1,1 +0,0 @@
-<svg class="prefix__logo" viewBox="0 0 128 128" width="24" height="24"><path fill="#42b883" d="M78.8 10L64 35.4 49.2 10H0l64 110 64-110H78.8z"/><path fill="#35495e" d="M78.8 10L64 35.4 49.2 10H25.6L64 76l38.4-66H78.8z"/></svg>
